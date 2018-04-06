26b32ab8f33eeaff5156a17da687f1b3399a70cb
Christie <cholvie@gmail.com>
2018-04-06 15:47:47 -0400

    changes i've been going through

--- assets/app.js
+++ assets/app.js
@@ -2,201 +2,97 @@
 //var newsSearch = 'news';
 var queryURL = 'https://newsapi.org/v2/top-headlines?sources=msnbc&apiKey=fe5d1157918b42018d8a665c5f6b1a06';
 
-$.ajax({url: queryURL, method: 'GET'})
+$.ajax({
+  url: queryURL,
+  method: 'GET'})
 .done(function(response) {
     console.log(response);
+    var data = response.articles;
+    console.log("author "  + data[0].author);
+});
+console.log("test");
+
+
+// javascript to grab the "search" results and display it to the screen
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAkvjtXklrCEduRMfooP-zLXpsfk0Qm5bM",
+    authDomain: "funfacts-newspage.firebaseapp.com",
+    databaseURL: "https://funfacts-newspage.firebaseio.com",
+    projectId: "funfacts-newspage",
+    storageBucket: "funfacts-newspage.appspot.com",
+    messagingSenderId: "250612511359"
+  };
+  firebase.initializeApp(config);
+  
+  var newsDb = firebase.database();
+
+  //reference message collection
+  var messagesRef = firebase.database().ref();
+
+//event listener for form submit
+document.getElementById('form-inline').addEventListener ('submit', submitForm);
+
+//submit form
+function submitForm(e) {
+  e.preventDefault();
+  
+  //get values
+  var userInput = getInputVal('userInput');
+  
+
+  //save message
+  saveMessage(userInput);
+
+  //show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  //hide alert after 3 seconds
+  setTimeout(function() {
+
+    //show alert
+  document.querySelector('.alert').style.display = 'none';
+
+  },3000);
+
+  //reset form
+  document.getElementById('form-inline').reset();
+
+}
+
+//function to get form values
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+//save the message to firebase
+function saveMessage(userInput, /*destination, firstTrain, frequency*/){
+  newsDb.ref().push({
+    userInput: userInput });
+
+
+}
+  newsDb.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  // Log everything that's coming out of snapshot
+  console.log("test");
+  console.log(childSnapshot);
+
+  console.log(childSnapshot.val().userInput);
+
+ var userInput = childSnapshot.val().userInput;
+
+  // Change the HTML to reflect
+
+  $("#resultTable > tbody").append("<tr><td>" + userInput + "</td></tr>");
+
+// Handle the errors
+
+}, function(errorObject) {
+
+  console.log("Errors handled: " + errorObject.code);
+
 });
 
 
-$(document).ready(function() {
 
-// Run this when button is clicked 
-  // Clear out previous results
-  // Create AJAX call using Giphy api url + value of button clicked
-  // Display gifs with that button value and their rating
-function gifDisplay() {
-
-  $('.results').empty();
-
-  // removes blank spaces in names and puts %20 in url
-  url = url + escape($(this).data('name'));
-  console.log(url);
-
-  // Creates AJAX call for the specific button being clicked
-  $.ajax({
-    url: url,
-    method: "GET"
-  })
-
-  // Reset URL Value after everything loads
-  url = 'https://newsapi.org/v2/top-headlines?sources=msnbc&apiKey=fe5d1157918b42018d8a665c5f6b1a06';
-}
-
-
-
-
-
-
-
-
-
-
-/*
-// the .on("click") function will start the AJAX call
-$(".newsSearch").on("click", function(event) {
-
-    //getting the text from the search box
-    var news = $(".form-group").val();
-
-    //construct the URL
-    var queryURL = 
-})
-
-
-
-
-
-
-
-
-
-
-
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
-
-  /*
-    // grab text the user typed into the search input, add as parameter to url
-    var searchTerm = $("newsSearch").val().trim();
-    queryURL += "&q=" + searchTerm;
-  
-    // if the user provides a startYear, include it in the queryURL
-    var startYear = $("#start-year").val().trim();
-  
-    if (parseInt(startYear)) {
-      queryURL += "&begin_date=" + startYear + "0101";
-    }
-  
-    // if the user provides an endYear, include it in the queryURL
-    var endYear = $("#end-year").val().trim();
-  
-    if (parseInt(endYear)) {
-      queryURL += "&end_date=" + endYear + "0101";
-    }
-  
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-  
-    return queryURL;
-  }
-  
-  /**
-   * takes API data (JSON/object) and turns it into elements on the page
-   * @param {object} NYTData - object containing NYT API data
-   */
-
-   /*
-  function updatePage(NYTData) {
-    // get from the form the number of results to display
-    // api doesn't have a "limit" parameter, so we have to do this ourselves
-    var numArticles = $("#article-count").val();
-  
-    // log the NYTData to console, where it will show up as an object
-    console.log(NYTData);
-    console.log("------------------------------------");
-  
-    // loop through and build elements for the defined number of articles
-    for (var i = 0; i < numArticles; i++) {
-  
-      // get specific article info for current index
-      var article = NYTData.response.docs[i];
-  
-      // increase the articleCount (track article # - starting at 1)
-      var articleCount = i + 1;
-  
-      // create the HTML well (section) and add the article content for each
-      var $articleWell = $("<article>");
-      $articleWell.addClass("well");
-      $articleWell.attr("id", "article-well-" + articleCount);
-  
-      // add the newly created element to the DOM
-      $("#well-section").append($articleWell);
-  
-      // if the article has a headline, log and append to $articleWell
-      var headline = article.headline;
-  
-      if (headline && headline.main) {
-        console.log(headline.main);
-  
-        $articleWell.append(
-          "<h3 class='articleHeadline'>" +
-          "<span class='label label-primary'>" + articleCount + "</span>" +
-          "<strong> " + headline.main + "</strong></h3>"
-        );
-      }
-  
-      // if the article has a byline, log and append to $articleWell
-      var byline = article.byline;
-  
-      if (byline && byline.original) {
-        console.log(byline.original);
-  
-        $articleWell.append("<h5>" + byline.original + "</h5>");
-      }
-  
-      // log section, and append to document if exists
-      var section = article.section_name;
-      console.log(article.section_name);
-      if (section) {
-        $articleWell.append("<h5>Section: " + section + "</h5>");
-      }
-  
-      // log published date, and append to document if exists
-      var pubDate = article.pub_date;
-      console.log(article.pub_date);
-      if (pubDate) {
-        $articleWell.append("<h5>" + article.pub_date + "</h5>");
-      }
-  
-      // append and log url
-      $articleWell.append(
-        "<a href='" + article.web_url + "'>" + article.web_url + "</a>"
-      );
-      console.log(article.web_url);
-    }
-  }
-  
-  // function to empty out the articles
-  function clear() {
-    $("#well-section").empty();
-  }
-  
-  // CLICK HANDLERS
-  // ==========================================================
-  
-  // .on("click") function associated with the Search Button
-  $("#run-search").on("click", function(event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
-  
-    // empty the region associated with the articles
-    clear();
-  
-    // build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-  
-    // make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // the data then gets passed as an argument to the updatePage function
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(updatePage);
-  });
-  
-  //  .on("click") function associated with the clear button
-  $("#clear-all").on("click", clear);
-  
