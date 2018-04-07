e0ddd541462bf26c7cee926283f27988437d416e
Mamie Smith <girlinmywhirl@gmail.com>
2018-04-07 03:51:37 -0400

    random making progress

---
+++ ffUpsideDown.js
@@ -0,0 +1,34 @@
+
+function upSideDwn() {
+   <div class= "ftembed">
+      <script src = "http://funtranslations.com/extensions/embed/v1/funtranslations.embed.js"></script>
+      <script>FunTranslations.Embed.render({translator: 'upside_down'});</script>
+   </div>
+}
+
+
+function paperTape() {
+   <div class="ftembed">
+      <script src = "http://funtranslations.com/extensions/embed/v1/funtranslations.embed.js" >< /script>
+      <script>FunTranslations.Embed.render({translator: 'papertape'});</script>
+   </div>
+}
+
+//
+//  I ain't got nobody...nobody...cares for me....nobody...cares for me...nobody. I-I-I-I'm so sad and lonely
+// ...sad and lonely...sad and lonely. won't you sweet mamma come and take a chance on me cause I ain't so bad.
+//
+function gunGan() {
+    <div class="ftembed">
+        <script src = "http://funtranslations.com/extensions/embed/v1/funtranslations.embed.js" >< /script>
+        <script>FunTranslations.Embed.render({translator: 'gungan'});</script>
+    </div>
+}
+
+
+function codeMorse() {
+    <div class="ftembed">
+       <script src = "http://funtranslations.com/extensions/embed/v1/funtranslations.embed.js" >< /script>
+       <script>FunTranslations.Embed.render({translator: 'morse'});</script>
+    </div>
+}
